df04bcf563622af99e3157aadaa840ecdb1a73d1
Quentin <dastuguequ@cy-tech.fr>
2022-04-08 11:19:15 +0200

    add interface authentificate

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -3,25 +3,21 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 public class Main extends Application {
 
-    public static void main(String[] args) {
-// write your code here
-        Application.launch(args);
-    }
     @Override
-    public void start(Stage primaryStage) {
-
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root,600,400);
-        primaryStage.setTitle("Library4J");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
