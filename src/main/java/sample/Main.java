b3f007e3b083913705c33d7da95a7de5a0e7f21f
Ilies <aitsouaril@cy-tech.fr>
2022-04-22 17:35:36 +0200

    User.fmxl a été refait, je fais le reste plus tard dans la soirée

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getResource("/user.fxml"));
-        Scene scene = new Scene(root, 600, 500);
+        Scene scene = new Scene(root, 1000, 700);
 
         primaryStage.setTitle("CY-Library");
         primaryStage.setScene(scene);
