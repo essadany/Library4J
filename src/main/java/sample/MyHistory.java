a24918e9ac7641e8713527645f3ada09fc482baf
Ilies <aitsouaril@cy-tech.fr>
2022-04-25 20:09:51 +0200

    MyHistory Update

--- src/main/java/sample/MyHistory.java
+++ src/main/java/sample/MyHistory.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MyHistory {
+    @FXML
+    private TableColumn<?, ?> AuthorBookCol;
+
+    @FXML
+    private TableColumn<?, ?> AvailBookCol;
+
+    @FXML
+    private TableColumn<?, ?> IdBookCol;
+
+    @FXML
+    private TableColumn<?, ?> NameBookCol;
+
+    @FXML
+    private Button backButtonProfile;
+
+    @FXML
+    private ChoiceBox<?> checkBoxBorrowing;
+
+    @FXML
+    private TextField searchBorrowingHistory;
+
+    @FXML
+    private Button searchButtonBorrowing;
+
+    @FXML
+    private TableView<?> tableBorrowingUser;
 }
