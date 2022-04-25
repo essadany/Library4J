59640fe2c4481a5ea789d7a0d270ddaa5bf9f076
Quentin <dastuguequ@cy-tech.fr>
2022-04-25 19:19:32 +0200

    Modifier SearchBook.fxml et SearchBookController

--- src/main/java/sample/SearchBookController.java
+++ src/main/java/sample/SearchBookController.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
+
 public class SearchBookController {
+
+    @FXML
+    private TableColumn<?, ?> AuthorBookCol;
+
+    @FXML
+    private TableColumn<?, ?> AvailBookCol;
+
+    @FXML
+    private TableColumn<?, ?> IdBookCol;
+
+    @FXML
+    private TableColumn<?, ?> NameBookCol;
+
+    @FXML
+    private Button ReturnBook;
+
+    @FXML
+    private ImageView ReturnButton;
+
+    @FXML
+    private Button SearchBookButton;
+
 }
