96dec11db517dc82dae7f61ff8ae8bfcf2f54ac3
essadany <essadanyya@cy-tech.fr>
2022-05-17 20:38:00 +0200

    add return book method

--- src/main/java/sample/borrow.java
+++ src/main/java/sample/borrow.java
@@ -1,18 +1,19 @@
 package sample;
 
+import java.sql.Date;
+
 public class borrow {
     private String bookID;
     private String title;
     private String author;
     private String edition_date;
-    private String userID;
+    private int userID;
     private String first_name;
     private String last_name;
-    private String status;
     private String issue_date;
     private String return_date;
     //constroctor
-    public borrow(String bookID, String title, String author, String edition_date, String userID, String first_name, String last_name, String status, String issue_date, String return_date) {
+    public borrow(String bookID, String title, String author, String edition_date, int userID, String first_name, String last_name, String issue_date, String return_date) {
         this.bookID = bookID;
         this.title = title;
         this.author = author;
@@ -20,8 +21,79 @@ public class borrow {
         this.userID = userID;
         this.first_name = first_name;
         this.last_name = last_name;
-        this.status = status;
         this.issue_date = issue_date;
+        this.return_date = return_date;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEdition_date() {
+        return edition_date;
+    }
+
+    public void setEdition_date(String edition_date) {
+        this.edition_date = edition_date;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getIssue_date() {
+        return issue_date;
+    }
+
+    public void setIssue_date(String issue_date) {
+        this.issue_date = issue_date;
+    }
+
+    public String getReturn_date() {
+        return return_date;
+    }
+
+    public void setReturn_date(String return_date) {
         this.return_date = return_date;
     }
 }
--- src/main/java/sample/manageLoans.java
+++ src/main/java/sample/manageLoans.java
@@ -1,29 +1,33 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
+import java.util.ResourceBundle;
 
-public class manageLoans {
+public class manageLoans implements Initializable {
     @FXML
     private TableColumn<?, ?> IdBookCol;
 
@@ -32,9 +36,6 @@ public class manageLoans {
 
     @FXML
     private Button ReturnButton2;
-
-    @FXML
-    private TableColumn<?, ?> author;
 
     @FXML
     private TableColumn<?, ?> author1;
@@ -92,21 +93,6 @@ public class manageLoans {
 
     @FXML
     private Button issue1issueButton;
-
-    @FXML
-    private ChoiceBox<?> issue2FilterChoiceBox;
-
-    @FXML
-    private TextField issue2SearchBar;
-
-    @FXML
-    private Button issue2SearchButton;
-
-    @FXML
-    private TableView<?> issue2SearchTableView;
-
-    @FXML
-    private Tab issue2Tab;
 
     @FXML
     private Button issue2returnButton;
@@ -186,8 +172,6 @@ public class manageLoans {
     @FXML
     private Button return2returnButton;
 
-    @FXML
-    private TableColumn<?, ?> titile;
 
     @FXML
     private TableColumn<?, ?> titile1;
@@ -256,7 +240,7 @@ public class manageLoans {
 
     public void borrow(ActionEvent event) throws SQLException {
         Connect con = new Connect();
-        PreparedStatement stat = con.connection().prepareStatement("insert into loans values  (1,?,?,?,NULL)");
+        PreparedStatement stat = con.connection().prepareStatement("insert into loans values  (DEFAULT,?,?,?,NULL)");
         stat.setString(1,userID.getText());
         stat.setString(2,bookID.getText());
         stat.setString(3, String.valueOf(issue1DateIssue.getValue()));
@@ -269,6 +253,131 @@ public class manageLoans {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText("Loan added succefuly !");
         alert.showAndWait();
+
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @FXML
+    private TextField issue2SearchBar;
+
+    @FXML
+    private Button issue2SearchButton;
+
+    @FXML
+    private TableView<borrow> issue2SearchTableView;
+
+    @FXML
+    private Tab issue2Tab;
+    @FXML
+    private TableColumn<borrow, String> issuedate;
+
+    @FXML
+    private TableColumn<borrow, String> lastname;
+
+    @FXML
+    private TableColumn<borrow, String> limitdate;
+    @FXML
+    private TableColumn<borrow, String> author;
+    @FXML
+    private TableColumn<borrow, String> title;
+    @FXML
+    private TableColumn<borrow, Integer> studentid;
+
+    @FXML
+    private TableColumn<borrow, String> bookid;
+
+    @FXML
+    private TableColumn<borrow, String> editiondate;
+
+    @FXML
+    private TableColumn<borrow, String> firstname;
+
+    @FXML
+    public ObservableList<borrow> data = FXCollections.observableArrayList();
+    @FXML
+    public void issueTable() throws IOException, ParserConfigurationException, SQLException, SAXException {
+        Connect conn = new Connect();
+        Statement stat = conn.connection().createStatement();
+        ResultSet res = stat.executeQuery("select * from loans");
+        while (res.next()) {
+            String url2 = "https://gallica.bnf.fr/services/OAIRecord?ark=" + res.getString(3);
+            System.out.println(url2);
+            URL obj = new URL(url2);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //print in String
+            // System.out.println(response.toString());
+            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                    .parse(new InputSource(new StringReader(response.toString())));
+            NodeList errNodes = doc.getElementsByTagName("oai_dc:dc");
+            Element err = (Element) errNodes.item(0);
+            // success
+            Statement statement = conn.connection().createStatement();
+            ResultSet res1 = statement.executeQuery("select * from users where userID = " + res.getInt(2));
+            if (res1.next()){
+                borrow loan = new borrow(res.getString(3), err.getElementsByTagName("dc:title").item(0).getTextContent(), err.getElementsByTagName("dc:publisher").item(0).getTextContent(), err.getElementsByTagName("dc:date").item(0).getTextContent(), res.getInt(2), res1.getString(2), res1.getString(3), res.getString(4), res.getString(5));
+                data.add(loan);
+            }
+
+        }
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        studentid.setCellValueFactory(new PropertyValueFactory<borrow, Integer>("userID"));
+        title.setCellValueFactory(new PropertyValueFactory<borrow, String>("title"));
+        author.setCellValueFactory(new PropertyValueFactory<borrow, String>("author"));
+        editiondate.setCellValueFactory(new PropertyValueFactory<borrow, String>("edition_date"));
+        firstname.setCellValueFactory(new PropertyValueFactory<borrow, String>("first_name"));
+        lastname.setCellValueFactory(new PropertyValueFactory<borrow, String>("last_name"));
+        bookid.setCellValueFactory(new PropertyValueFactory<borrow, String>("bookID"));
+        issuedate.setCellValueFactory(new PropertyValueFactory<borrow, String>("issue_date"));
+        limitdate.setCellValueFactory(new PropertyValueFactory<borrow, String>("return_date"));
+
+        issue2SearchTableView.setItems(data);
+
+
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @FXML
+    public void returnBook(ActionEvent event) throws SQLException {
+        Connect conn = new Connect();
+        PreparedStatement stat = conn.connection().prepareStatement("select * from loans where userID =? and bookID = ? and date_Return=date_Borrow");
+        stat.setString(1, return1Studentid.getText());
+        stat.setString(2, return1Bookid.getText());
+        ResultSet res = stat.executeQuery();
+        if (res.next()){
+            if(return1ReturnDate.getValue().equals("")){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("please enter return Date!");
+                alert.showAndWait();
+            }else{
+                PreparedStatement statement = conn.connection().prepareStatement("update loans set date_Return = ? where idLoan = ?");
+                statement.setString(1, String.valueOf(return1ReturnDate.getValue()));
+                statement.setString(2, String.valueOf(res.getInt(1)));
+                statement.execute( );
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("retrun book succed");
+                alert.showAndWait();
+
+            }
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("please verify the informations, There is no loan assigned !");
+            alert.showAndWait();
+        }
+
 
     }
 }
